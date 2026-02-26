f0761f20d8ff7d23357b8022de24a2917f81b838
Lipe :) <felipecazaes23@gmail.com>
2026-02-26 14:40:09 -0300

    Explicando a função do if

--- src/ParouImpar.java
+++ src/ParouImpar.java
@@ -6,7 +6,7 @@ public class ParouImpar {
         System.out.println("Informe o primeiro numero");
         int numUm = leitura.nextInt();
 
-        if (numUm % 2 == 0){
+        if (numUm % 2 == 0){ // Aqui é claro. Se numa divisão com 2 o resto do calculo ser exato, no caso 0, o número é par. Caso não, é impar.
             System.out.println("O numero é par!");
         } else {
             System.out.println("O numero é impar!");
