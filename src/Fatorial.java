b1a1ef6e431288b74ab2fe6bbd7bdd2045dfff5d
Lipe :) <felipecazaes23@gmail.com>
2026-02-26 14:39:04 -0300

    Explicando a função da variável fatorial no código.

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -7,7 +7,7 @@ public class Fatorial {
         int fatorial = 1;
 
         for (int i = 1; i <= num ; i++) {
-            fatorial *= i;
+            fatorial *= i; // A lógica aqui é armazenar e calcular, de acordo com o percorrer do for, a variável irá armazenar o resultado da multiplicação anterior e multiplicará com o próximo número.
         }
         System.out.println("fatorial de " + num + " é: " + fatorial);
     }
