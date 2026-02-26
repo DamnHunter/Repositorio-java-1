2628a9fc2d54b24f57569488b1fe52421113a24c
Lipe :) <felipecazaes23@gmail.com>
2026-02-26 14:41:19 -0300

    Explicando como a tabuada funciona

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -6,8 +6,8 @@ public class Tabuada {
         Scanner leitura = new Scanner(System.in);
         int num = leitura.nextInt();
         int multi;
-        for (int i = 1; i <= 10; i++) {
-            multi = num * i;
+        for (int i = 1; i <= 10; i++) { // o for seria um comum já que o que mudaria seria apenas a entrada de dados do usuário.
+            multi = num * i; // Esta variável seria apenas para armazenar o resultado da multiplicação.
             System.out.println(num + " x " + i + " = " + multi);
         }
     }
